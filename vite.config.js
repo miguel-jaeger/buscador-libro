ddffb6402faff2a01bb4a52785359560e9210300
miguel-jaeger <miguel.jaeger@gmail.com>
2026-09-25 15:29:02 -0500

    feat: add OPDS proxy for Gutenberg in Vite dev server

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { gutenbergProxy } from './gutenberg-proxy.js'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), gutenbergProxy()],
 })
